94ab818768751a5795a2ad0ec5fbdb201547c8ef
ChaeeunShin <shince0805@gmail.com>
2024-01-23 18:20:39 +0900

    [Setting] 프로젝트 생성

---
+++ MyWeatherApp/MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Chaeeun Shin on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
