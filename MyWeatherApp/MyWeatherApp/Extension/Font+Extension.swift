b895b8fb811a751acb33713f30d8593c8db580c4
ChaeeunShin <shince0805@gmail.com>
2024-01-24 22:32:19 +0900

    [Design] 폰트 설정

---
+++ MyWeatherApp/MyWeatherApp/Extension/Font+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Font+Extension.swift
+//  MyWeatherApp
+//
+//  Created by Chaeeun Shin on 1/24/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let cityTitle = Font.system(size: 20, weight: .semibold)
+    static let temperature = Font.system(size: 100, weight: .regular)
+    static let discription = Font.system(size: 20, weight: .regular)
+    static let infoTitle = Font.system(size: 16, weight: .regular)
+    static let infoContent = Font.system(size: 22, weight: .regular)
+}
