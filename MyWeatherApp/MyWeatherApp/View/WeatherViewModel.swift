a6cfdc559f9db890a3155cc88c9619c793f47f4c
ChaeeunShin <shince0805@gmail.com>
2024-01-25 14:00:30 +0900

    [Chore] 변수 대입 위치 변경

--- MyWeatherApp/MyWeatherApp/View/WeatherViewModel.swift
+++ MyWeatherApp/MyWeatherApp/View/WeatherViewModel.swift
@@ -38,22 +38,22 @@ class WeatherViewModel: ObservableObject {
         do {
             try loadCity()
             print("파일 불러오기 완료!")
+            
+            if isFirstRun {     // 첫 실행일 경우
+                sido = "서울특별시"
+                city = cityList["서울특별시"]![0]    // 강남구
+                
+                isFirstRun.toggle()
+            } else {
+                sido = storedSido
+                city = storedCity.convertToCity()
+            }
         } catch CSVParseError.notFound {
             print("파일을 찾을 수 없습니다.")
         } catch CSVParseError.invalidFile {
             print("잘못된 형식의 파일입니다.")
         } catch {
             print("알 수 없는 에러 발생!!")
-        }
-        
-        if isFirstRun {     // 첫 실행일 경우
-            sido = "서울특별시"
-            city = cityList["서울특별시"]![0]    // 강남구
-            
-            isFirstRun.toggle()
-        } else {
-            sido = storedSido
-            city = storedCity.convertToCity()
         }
     }
     
